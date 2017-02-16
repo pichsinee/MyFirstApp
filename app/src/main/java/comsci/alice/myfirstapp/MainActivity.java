b12909392ed02879385b0280584d52617706bee2
pichsinee <p.pichsinee@gmail.com>
2017-02-16 11:33:25 +0700

    Button MoreInfo Complete

--- app/src/main/java/comsci/alice/myfirstapp/MainActivity.java
+++ app/src/main/java/comsci/alice/myfirstapp/MainActivity.java
@@ -1,7 +1,10 @@
 package comsci.alice.myfirstapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }   //Main Method onCreate
+
+    public void onClickMoreInfo(View view) {
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.dlt.go.th/th/dlt-knowledge/view.php?_did=111"));
+        startActivity(intent);
+
+    }   //Main Method onClickMoreInfo
+
+
+}   // Main Class
