3328d47f04ffe91ebc0ee5858d2f0653eb044572
pichsinee <p.pichsinee@gmail.com>
2017-03-16 11:51:07 +0700

    Test1 Complete

--- app/src/main/java/comsci/alice/myfirstapp/AboutMe.java
+++ app/src/main/java/comsci/alice/myfirstapp/AboutMe.java
@@ -1,18 +1,58 @@
 package comsci.alice.myfirstapp;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class AboutMe extends AppCompatActivity {
+
+    //Explicit
+    MediaPlayer mediaPlayer;
+    int soundID = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+
+        //Toast onClick
+        ImageView clickImageView = (ImageView) findViewById(R.id.imvMyPic);
+        clickImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getApplicationContext(), "Hello! My name is Alice" , Toast.LENGTH_SHORT).show();
+
+                //Show sound1 when click image
+                soundID = R.raw.sound01;
+                playSound(soundID);
+
+            }
+        });
+
+
+        //Toast onLongClick
+        ImageView longClickImageView = (ImageView) findViewById(R.id.imvMyPic);
+        longClickImageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+
+                Toast.makeText(getApplicationContext(), "Thanks Guys!\nHope you like the My First App", Toast.LENGTH_SHORT).show();
+                //Show sound1 when click image
+                soundID = R.raw.sound02;
+                playSound(soundID);
+
+                return true;
+            }
+        });
+
 
         //link to facebook
         TextView fbTextView = (TextView) findViewById(R.id.txtFacebook);
@@ -26,6 +66,13 @@ public class AboutMe extends AppCompatActivity {
         });
 
     }//main method
+
+    private void playSound(int soundID) {
+
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), soundID);
+        mediaPlayer.start();
+    }
 
     public void onClickBack(View view){
         finish();
